4aae0be800db0dc457d9ca834a0930242b7b385f
Tom Booth <tombooth@gmail.com>
2019-07-10 12:14:47 +0100

    Translate GitHub API response into Repository entity

--- lib/github.rb
+++ lib/github.rb
@@ -1,6 +1,9 @@
 require 'base64'
 require 'json'
 require 'octokit'
+require 'yaml'
+
+TAG_PREFIX="cloud.rvu.ontology"
 
 class GitHub
 
@@ -15,11 +18,48 @@ class GitHub
       begin
         metadata_resp = client.contents(repo.full_name, path: ".rvu/metadata")
 
-        repo_h[:metadata] = Base64.decode64(metadata_resp.content)
+        repo_h[:metadata] = YAML.load(Base64.decode64(metadata_resp.content))
       rescue Octokit::NotFound
       end
 
-      repo_h
+      relations = []
+
+      if repo_h.has_key?(:metadata) and repo_h[:metadata].has_key?(:tags)
+        repo_h[:metadata][:tags].each { |tag, val|
+          return if not tag.starts_with TAG_PREFIX
+
+          type = tag[TAG_PREFIX.length..-1]
+          relations.push(
+            {
+              metadata: {
+                type: type,
+              },
+              properties: {
+                other: val
+              },
+            }
+          )
+        }
+
+        puts relations
+      end
+
+      {
+        path: "/repository/github.com/#{repo.full_name}.json",
+        entity: {
+          metadata: {
+            type: "/entities/v1/repository",
+          },
+          relations: relations,
+          properties: {
+            language: repo[:language],
+            license: repo.has_key?(:license) ? repo[:license][:key] : nil,
+            created_at: repo[:created_at],
+            updated_at: repo[:updated_at],
+            pushed_at: repo[:pushed_at],
+          },
+        },
+      }
     }
   end
 end

6dd92de3610b08ec1222a817c460768c3d72916e
Tom Booth <tombooth@gmail.com>
2019-07-15 11:41:55 +0100

    Pull down relationships and entities from registry

--- lib/docker.rb
+++ lib/docker.rb
@@ -1,4 +1,6 @@
 require 'httparty'
+require 'ruby-progressbar'
+require 'uri'
 
 #puts response.body, response.code, response.message, response.headers.inspect
 
@@ -20,41 +22,100 @@ class DockerRegistry
   base_uri "https://registry.usw.co"
 
   def sync
-    registry.repositories.map { |repo|
-      registry.tags(repo).map { |tag|
-        manifest = registry.manifest(repo, tag)
-        {
-          name: "#{repo}:#{tag}",
-          created: registry.created_from(manifest),
-          labels: registry.labels_from(manifest),
-        }
+    puts "Loading all repositories"
+    Parallel.map(repositories, progress: "Getting all repositories", in_processes: 20) { |repo|
+      Parallel.map(tags(repo)) { |tag|
+        begin
+          digest, manifest = manifest(repo, tag)
+
+          path = "/container/registry.usw.co/#{repo}/#{digest}"
+          labels = labels_from(manifest)
+          relations = []
+
+
+          if labels.has_key?("org.label-schema.vcs-url")
+            vcs_uri = URI(labels["org.label-schema.vcs-url"])
+
+            without_ext = File.join(File.dirname(vcs_uri.path), File.basename(vcs_uri.path, ".*"))
+
+            relations << {
+              metadata: {
+                type: "/relation/v1/was_built_by",
+              },
+              properties: {
+                a: path,
+                b: "/repository/#{vcs_uri.host}#{without_ext}",
+                ref: labels["org.label-schema.vcs-ref"],
+                at: labels["org.label-schema.build-date"],
+              }
+            }
+          end
+
+          {
+            path: path,
+            symlinks: [
+              "/container/registry.usw.co/#{repo}/#{tag}",
+            ],
+            entity: {
+              metadata: {
+                type: "/entities/v1/container",
+              },
+              properties: {
+                provider: "docker",
+                created_at: created_from(manifest),
+              },
+            },
+            relations: relations,
+          }
+        rescue StandardError => e
+          $stderr.puts manifest
+          $stderr.puts e.message
+          $stderr.puts e.backtrace.inspect
+          {}
+        end
       }
     }.flatten
   end
 
   def repositories
-    list('/v2/_catalog', 'repositories')
+    list('/v2/_catalog?n=1000000', 'repositories')
   end
 
   def tags(repo)
-    list("/v2/#{repo}/tags/list", 'tags')
+    list("/v2/#{repo}/tags/list?n=1000000", 'tags')
   end
 
   def manifest(repo, ref)
     manifest = self.class.get(
       "/v2/#{repo}/manifests/#{ref}", {
-        headers: { "Accept" => "application/vnd.docker.distribution.manifest.v2+json" },
+        headers: { "Accept" => "application/vnd.docker.distribution.manifest.v2+json, application/vnd.docker.distribution.manifest.v1+json" },
         format: :json,
       })
 
-    config = self.class.get(
-      "/v2/#{repo}/blobs/#{manifest["config"]["digest"]}", {
-        headers: { "Accept" => "application/vnd.docker.distribution.manifest.v2+json" },
-        format: :json,
-      })
+    if manifest["config"]
+      manifest_digest = "sha256:#{Digest::SHA256.hexdigest manifest.body}"
+    else
+      #$stderr.puts "WARN: v2.1 manifest, cannot calculate digest so relying on server"
+      manifest_digest = manifest.header["Docker-Content-Digest"]
+    end
 
-    config["schemaVersion"] = 2
-    config
+    if manifest_digest != manifest.header["Docker-Content-Digest"]
+      puts "[#{repo}:#{ref}] #{manifest.header["Docker-Content-Digest"]} != #{manifest_digest}"
+    end
+
+    if manifest["config"]
+      config = self.class.get(
+        "/v2/#{repo}/blobs/#{manifest["config"]["digest"]}", {
+          headers: { "Accept" => "application/vnd.docker.distribution.manifest.v2+json" },
+          format: :json,
+        })
+
+      config["schemaVersion"] = 2
+    else
+      config = manifest
+    end
+
+    return manifest_digest, config
   end
 
   def labels_from(manifest)
@@ -63,7 +124,7 @@ class DockerRegistry
       manifest["history"].map { |layer|
         compat = JSON.parse(layer["v1Compatibility"])
         compat["container_config"]["Labels"]
-      }.uniq
+      }.compact.reduce(&:merge)
     when 2
       cc_l = manifest["container_config"]["Labels"] || {}
       c_l = manifest["config"]["Labels"] || {}
@@ -93,7 +154,9 @@ class DockerRegistry
     begin
       response = self.class.get(path)
       list = list + JSON.parse(response.body)[key]
-      return list
+    rescue StandardError => e
+      $stderr.puts e.message
+      $stderr.puts e.backtrace.inspect
     end while path = next_link(response)
 
     list

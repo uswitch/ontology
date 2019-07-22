1c558f4729b10bf27081af46a33d0ab1e665b31a
Tom Booth <tombooth@gmail.com>
2019-07-22 10:24:25 +0100

    Pull people from Azure AD

--- lib/external/ad.rb
+++ lib/external/ad.rb
@@ -20,8 +20,46 @@ class AD
     client = Azure::GraphRbac::V1_6::GraphRbacClient.new(credentials)
     client.tenant_id = "1181c46f-f004-40a4-959a-2b630e7852df"
 
-    client.users.list.map { |user|
-      client.serialize(Azure::GraphRbac::V1_6::Models::User.mapper, user)
+    path = "/#{client.tenant_id}/users"
+    users = []
+
+    while path do
+      result = client.make_request(:get, path, { query_params: {'api-version' => '1.6'} })
+
+      users += result["value"]
+
+      if result.has_key?("odata.nextLink")
+        path = "/#{client.tenant_id}/#{result["odata.nextLink"]}"
+      else
+        path = nil
+      end
+    end
+
+    users
+      .select { |u| u["accountEnabled"] and u["userType"] == "Member" }
+      .map { |u|
+
+      upn = u["userPrincipalName"].downcase
+      user, domain = upn.downcase.split("@")
+
+      path = "/person/#{domain}/#{user}"
+
+      {
+        path: path,
+        symlinks: [
+          "/person/by-email/#{upn}",
+          "/person/by-id/#{u["objectId"]}",
+        ],
+        entity: {
+          metadata: {
+            type: "/entity/v1/person",
+          },
+          properties: {
+            id: u["objectId"],
+            email: u["upn"],
+          },
+        },
+      }
     }
   end
 

c9739c9a1daed1ba48190660a9b3954d287298ae
Tom Booth <tombooth@gmail.com>
2019-08-19 18:58:03 +0100

    Switch to our own handler not graphql-go/handler
    It doesn't work with Apollo, as Apollo sends an array of queries.
    Means we can add audit logging too

--- cmd/http-api/api.go
+++ cmd/http-api/api.go
@@ -3,10 +3,11 @@ package main
 import (
 	"encoding/json"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 
-	"github.com/graphql-go/handler"
+	graphqlgo "github.com/graphql-go/graphql"
 
 	"github.com/uswitch/ontology/pkg/audit"
 	"github.com/uswitch/ontology/pkg/authnz"
@@ -15,6 +16,11 @@ import (
 	"github.com/uswitch/ontology/pkg/store"
 )
 
+type RequestOptions struct {
+    Query         string                 `json:"query" url:"query" schema:"query"`
+    Variables     map[string]interface{} `json:"variables" url:"variables" schema:"variables"`
+    OperationName string                 `json:"operationName" url:"operationName" schema:"operationName"`
+}
 
 func apiHandler(s store.Store, authn authnz.Authenticator, auditLogger audit.Logger, cors middleware.Middleware) (http.Handler, error) {
 	apiMux := http.NewServeMux()
@@ -58,14 +64,45 @@ func apiHandler(s store.Store, authn authnz.Authenticator, auditLogger audit.Log
 		return nil, err
 	}
 
-	h := handler.New(&handler.Config{
-		Schema: schema,
-		Pretty: true,
-		GraphiQL: false,
-		Playground: true,
-	})
+	apiMux.HandleFunc("/graphql", func(w http.ResponseWriter, r *http.Request) {
+		bodyBytes, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			w.WriteHeader(400)
+			return
+		}
 
-	apiMux.Handle("/graphql", h)
+		var opts RequestOptions
+		err = json.Unmarshal(bodyBytes, &opts)
+		if err != nil {
+			// try an array, it's what apollo sends
+			var manyOpts []RequestOptions
+			err = json.Unmarshal(bodyBytes, &manyOpts)
+			if err != nil || len(manyOpts) == 0 {
+				w.WriteHeader(400)
+				return
+			}
+
+			opts = manyOpts[0]
+		}
+
+		ctx := r.Context()
+
+		auditLogger.Log(ctx, audit.AuditData{
+			"query": opts.Query,
+			"variables": opts.Variables,
+			"operation_name": opts.OperationName,
+		})
+
+		result := graphqlgo.Do(graphqlgo.Params{
+			Schema:         *schema,
+			RequestString:  opts.Query,
+			VariableValues: opts.Variables,
+			OperationName:  opts.OperationName,
+			Context:        ctx,
+		})
+
+		json.NewEncoder(w).Encode(result)
+	})
 
 	return middleware.Wrap(
 		[]middleware.Middleware{
--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ require (
 	github.com/coreos/go-oidc v2.0.0+incompatible
 	github.com/google/uuid v1.1.1
 	github.com/graphql-go/graphql v0.7.8
-	github.com/graphql-go/handler v0.2.3
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
 	github.com/qri-io/jsonschema v0.1.1
 	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4 // indirect
